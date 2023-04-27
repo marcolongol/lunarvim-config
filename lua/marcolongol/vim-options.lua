c285d00749a3ab2fe74d963b94ba709963a6ddb2
Lucas Marcolongo <lucas_marco@live.com>
2023-04-27 15:24:01 -0700

    fix: set cmd as default shell

--- lua/marcolongol/vim-options.lua
+++ lua/marcolongol/vim-options.lua
@@ -1,12 +1,12 @@
 -- Enable powershell as your default shell
-vim.opt.shell = "pwsh.exe -NoLogo"
-vim.opt.shellcmdflag =
-"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.cmd [[
-		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-		set shellquote= shellxquote=
-  ]]
+-- vim.opt.shell = "pwsh.exe -NoLogo"
+-- vim.opt.shellcmdflag =
+-- "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+-- vim.cmd [[
+-- 		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+-- 		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+-- 		set shellquote= shellxquote=
+--   ]]
 
 -- Set a compatible clipboard manager
 vim.g.clipboard = {
