c7d754fc7aa3324749aba0f280ebc212b7f1fb3e
Lucas Marcolongo <lucas_marco@live.com>
2023-04-28 10:39:30 -0700

    remove: hop

--- lua/marcolongol/plugins.lua
+++ lua/marcolongol/plugins.lua
@@ -52,17 +52,6 @@ lvim.plugins = {
     { "tpope/vim-surround" },
     { "tpope/vim-commentary" },
 
-    -- hop
-    {
-        "phaazon/hop.nvim",
-        event = "BufRead",
-        config = function()
-            require("hop").setup()
-            vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-            vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
-        end,
-    },
-
     -- bqf
     {
         "kevinhwang91/nvim-bqf",
