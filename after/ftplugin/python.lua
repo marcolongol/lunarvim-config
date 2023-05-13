72973dff38575f7a72afee9ac3c7020c5d40d887
Lucas Marcolongo <lucas_marco@live.com>
2023-05-12 18:35:54 -0700

    fix: increase line-length for python linting tools

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -11,9 +11,11 @@ local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
 	{
 		name = "black",
+		args = { "--max-line-length", "87" },
 	},
 	{
 		name = "isort",
+		args = { "--max-line-length", "87" },
 	},
 })
 
@@ -22,14 +24,17 @@ local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
 	{
 		name = "flake8",
+		args = { "--max-line-length", "87" },
 	},
 	{
 		name = "mypy",
 	},
 	{
 		name = "pylint",
+		args = { "--max-line-length", "87" },
 	},
 	{
 		name = "ruff",
+		args = { "--fix", "--max-line-length", "87" },
 	},
 })
