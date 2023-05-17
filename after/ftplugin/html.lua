e6fcd9a2d68fc33ff5c9a4566e9b46670ace08a5
Lucas Marcolongo <lucas_marco@live.com>
2023-05-17 15:32:23 -0700

    add: emmet support for html

---
+++ after/ftplugin/html.lua
@@ -0,0 +1,19 @@
+-- lsp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+local opts = {
+	capabilities = capabilities,
+	filetypes = {
+		"html",
+		"css",
+		"scss",
+		"less",
+	},
+	init_options = {
+		html = {
+			options = {
+				-- For possible options check https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+			},
+		},
+	},
+}
+require("lvim.lsp.manager").setup("emmet_ls", opts)
