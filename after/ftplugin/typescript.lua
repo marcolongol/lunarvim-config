d744601857487bc86c7a9fc09d0f40940326e1e4
Lucas Marcolongo <lucas_marco@live.com>
2023-05-26 13:27:50 -0700

    add: typescript linting+formatting

---
+++ after/ftplugin/typescript.lua
@@ -0,0 +1,15 @@
+-- typescript formatters
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+    {
+        name = "prettierd",
+    },
+})
+
+-- typescript linting
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+    {
+        name = "eslint",
+    },
+})
