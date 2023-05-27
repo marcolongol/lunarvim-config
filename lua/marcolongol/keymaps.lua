bdb7a56f5ca81721cad6366a21b8431f2e5c620e
Lucas Marcolongo <lucas_marco@live.com>
2023-05-26 20:26:06 -0700

    add: telescope trouble keymaps

--- lua/marcolongol/keymaps.lua
+++ lua/marcolongol/keymaps.lua
@@ -9,35 +9,35 @@ lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 vim.g.bufferline = {
-	animation = true,
-	auto_hide = true,
-	closable = true,
-	clickable = true,
-	icon_close_tab = "❌",
-	icon_close_tab_modified = "●",
-	icon_separator_active = "▎",
-	icon_separator_inactive = "▎",
-	icon_pinned = "📌",
-	maximum_padding = 4,
-	maximum_length = 30,
-	semantic_letters = true,
-	no_name_title = nil,
+    animation = true,
+    auto_hide = true,
+    closable = true,
+    clickable = true,
+    icon_close_tab = "❌",
+    icon_close_tab_modified = "●",
+    icon_separator_active = "▎",
+    icon_separator_inactive = "▎",
+    icon_pinned = "📌",
+    maximum_padding = 4,
+    maximum_length = 30,
+    semantic_letters = true,
+    no_name_title = nil,
 }
 
 lvim.lsp.buffer_mappings.normal_mode["gr"] = {
-	":lua require('telescope.builtin').lsp_references()<CR>",
+    ":lua require('telescope.builtin').lsp_references()<CR>",
 }
 
 lvim.lsp.buffer_mappings.normal_mode["gd"] = {
-	":lua require('telescope.builtin').lsp_definitions()<CR>",
+    ":lua require('telescope.builtin').lsp_definitions()<CR>",
 }
 
 lvim.lsp.buffer_mappings.normal_mode["gD"] = {
-	":lua require('telescope.builtin').type_definition()<CR>",
+    ":lua require('telescope.builtin').type_definition()<CR>",
 }
 
 lvim.lsp.buffer_mappings.normal_mode["gf"] = {
-	":Telescope frecency<CR>",
+    ":Telescope frecency<CR>",
 }
 
 -- move selection
@@ -51,21 +51,32 @@ lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Project
 -- telescope bindings
 -- zoxide
 lvim.builtin.which_key.mappings["z"] = {
-	name = "Zoxide",
-	l = { "<cmd>lua require('telescope').extensions.zoxide.list{}<CR>", "zoxide list" },
-	a = { "<cmd>lua require('telescope').extensions.zoxide.list{cmd = 'zoxide add'}<CR>", "Add" },
-	r = { "<cmd>lua require('telescope').extensions.zoxide.list{cmd = 'zoxide remove'}<CR>", "Remove" },
+    name = "Zoxide",
+    l = { "<cmd>lua require('telescope').extensions.zoxide.list{}<CR>", "zoxide list" },
+    a = { "<cmd>lua require('telescope').extensions.zoxide.list{cmd = 'zoxide add'}<CR>", "Add" },
+    r = { "<cmd>lua require('telescope').extensions.zoxide.list{cmd = 'zoxide remove'}<CR>", "Remove" },
 }
 -- neoclip
 lvim.builtin.which_key.mappings["y"] = {
-	name = "Neoclip",
-	y = { "<cmd>lua require('telescope').extensions.neoclip.default()<CR>", "List" },
-	p = { "<cmd>lua require('telescope').extensions.neoclip.pasteboard()<CR>", "Pasteboard" },
+    name = "Neoclip",
+    y = { "<cmd>lua require('telescope').extensions.neoclip.default()<CR>", "List" },
+    p = { "<cmd>lua require('telescope').extensions.neoclip.pasteboard()<CR>", "Pasteboard" },
 }
 
 -- color-picker (color-picker.nvim)
 lvim.builtin.which_key.mappings["C"] = {
-	name = "Color Picker",
-	c = { "<cmd>PickColor<CR>", "Pick Color" },
-	i = { "<cmd>PickColorInsert<CR>", "Pick Color Insert" },
+    name = "Color Picker",
+    c = { "<cmd>PickColor<CR>", "Pick Color" },
+    i = { "<cmd>PickColorInsert<CR>", "Pick Color Insert" },
+}
+
+-- trouble
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Trouble",
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Document Diagnostics" },
+  l = { "<cmd>Trouble loclist<cr>", "Location List" },
+  q = { "<cmd>Trouble quickfix<cr>", "Quickfix" },
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 }
