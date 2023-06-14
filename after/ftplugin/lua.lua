782d80d83a18cbbcc3b385f859681e08f07938ee
Lucas Marcolongo <lucas_marco@live.com>
2023-06-14 15:38:47 -0700

    fix: lua support

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -9,7 +9,25 @@ formatters.setup({
 -- lua linting
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
+  -- {
+  --   name = "selene",
+  -- },
   {
     name = "luacheck",
   },
 })
+
+-- lsp
+local opts = {
+  on_attach = require("lvim.lsp").common_on_attach,
+  filetypes = { "lua" },
+  root_dir = require("lspconfig/util").root_pattern(".git", vim.fn.getcwd()),
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+}
+require("lvim.lsp.manager").setup("lua_ls", opts)
