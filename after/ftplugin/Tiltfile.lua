2824720e3d835a5979950578db58b2316988df88
Lucas Marcolongo <lucas_marco@live.com>
2023-06-27 11:25:06 -0700

    fix: Tiltfile support

--- after/ftplugin/Tiltfile.lua
+++ after/ftplugin/Tiltfile.lua
@@ -1,3 +1,5 @@
+local util = require("lspconfig/util")
+
 -- Tiltfile formatters
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
@@ -15,26 +17,17 @@ formatters.setup({
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
   {
-    name = "flake8",
-    filetypes = { "Tiltfile" },
-  },
-  {
-    name = "mypy",
-    filetypes = { "Tiltfile" },
-  },
-  {
     name = "pylint",
-    filetypes = { "Tiltfile" },
-  },
-  {
-    name = "ruff",
-    filetypes = { "Tiltfile" },
   },
 })
 
 -- Setup lsp for Tiltfile
 local opts = {
+  cmd = { "tilt", "lsp", "start", "--debug", "--verbose" },
   filetypes = { "Tiltfile" },
+  root_dir = function(fname)
+    return util.root_pattern(".git", "Tiltfile")(fname) or util.path.dirname(fname)
+  end,
 }
 
 require("lvim.lsp.manager").setup("pyright", opts) -- HACK: pyright as proxy
