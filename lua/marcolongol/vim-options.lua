e184c83b600e19c290dd4e113fda69d0b20399bc
Lucas Marcolongo <lucas_marco@live.com>
2024-02-12 17:20:07 -0800

    feat: set powershell as default shell

--- lua/marcolongol/vim-options.lua
+++ lua/marcolongol/vim-options.lua
@@ -1,13 +1,13 @@
 -- SECTION: Powershell config
 -- Enable powershell as your default shell
--- vim.opt.shell = "pwsh.exe -NoLogo"
--- vim.opt.shellcmdflag =
--- "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
--- vim.cmd [[
--- 		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
--- 		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
--- 		set shellquote= shellxquote=
---   ]]
+vim.opt.shell = "pwsh.exe -NoLogo"
+vim.opt.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.cmd([[
+		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+  ]])
 
 -- Set a compatible clipboard manager
 vim.g.clipboard = {
@@ -42,7 +42,7 @@ vim.opt.guifont = "JetBrainsMono Nerd Font:h09"
 
 -- line numbers
 vim.opt.nu = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 -- tab options
 vim.opt.tabstop = 4
